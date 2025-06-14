02917f3b6e094efcbd90816cd59c1a84de2d20db
rakesh-gyawali <rakeshgyawali@gmail.com>
2025-06-14 11:28:00 +0900

    fixed join

--- rakesh/sql/Oracle_HR_Schema_qn/most_recent_job_change_for_each_employee.sql
+++ rakesh/sql/Oracle_HR_Schema_qn/most_recent_job_change_for_each_employee.sql
@@ -5,48 +5,28 @@
 -- ・New job title
 -- ・Start date
 -- ・Department name
-
 -- my first solution: without using rank over
 --recent_job(rj): 
 with recent_job as (
-    select 
-        e.employee_id
-        , e.first_name || ' ' || e.last_name as full_name
-        , max(jh.start_date) last_job_start_date
-    from 
+    select
+        e.employee_id,
+        e.first_name || ' ' || e.last_name as full_name,
+        max(jh.start_date) last_job_start_date
+    from
         oehr_job_history jh
-    inner join 
-        oehr_employees e
-    on 
-        jh.employee_id = e.employee_id
-    group by 
-        e.employee_id, e.first_name || ' ' || e.last_name
+        inner join oehr_employees e on jh.employee_id = e.employee_id
+    group by
+        e.employee_id,
+        e.first_name || ' ' || e.last_name
 )
-select 
-   rj.full_name
-  , j.job_title
-   , jh.start_date
-   , d.department_name
-from 
-    oehr_departments d
-inner join 
-    oehr_employees e
-on 
-    d.department_id = e.department_id
-inner join 
-    oehr_jobs j
-on 
-    j.job_id = e.job_id
-inner join 
-    oehr_job_history jh
-on 
-    jh.job_id = j.job_id
-inner join 
+select
+    rj.full_name,
+    j.job_title,
+    jh.start_date,
+    d.department_name
+from
     recent_job rj
-on 
-    rj.employee_id = e.employee_id -- TODO: Check whether join is correct
-and 
-    rj.employee_id = jh.employee_id 
-and 
-    jh.start_date = rj.last_job_start_date
-;
+inner join oehr_job_history jh on rj.employee_id = jh.employee_id 
+    and jh.start_date = rj.last_job_start_date
+inner join oehr_jobs j on j.job_id = jh.job_id
+left join oehr_departments d on d.department_id = jh.department_id;
